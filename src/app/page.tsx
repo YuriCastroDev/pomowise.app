b35b6b0a990f5b161f9bd5fdbf6cc6043dd37e82
YuriCastroDev <yuri.castro.dev@gmail.com>
2024-03-26 20:19:34 -0300

    Commit inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Pomowise</h1>
+        <hr></hr>
+      </div>
+      <div>
+        25:00
+      </div>
+      <div>
+        Start
+      </div>
+    </main>
+  );
+}
